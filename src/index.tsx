791b96f183b636924f552b68d04da9a6bbafef67
alemarnery <aleemar.95@gmail.com>
2020-10-07 21:38:53 -0800

    index.ts

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers";
+
+interface AppProps {
+  color: string;
+}
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App color="red" />
+  </Provider>,
+  document.querySelector("#root")
+);
